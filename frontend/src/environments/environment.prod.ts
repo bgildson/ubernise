fdde01fc04e66425598b5c7fab892192f42ca55d
bgildson <gildson13@hotmail.com>
2018-10-01 20:58:33 -0300

    initial

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCJx68RrFWcwV4lLubbS9WQGsMuKgJMa3o",
+        authDomain: "ubernisee.firebaseapp.com",
+        databaseURL: "https://ubernisee.firebaseio.com",
+        projectId: "ubernisee",
+        storageBucket: "ubernisee.appspot.com",
+        messagingSenderId: "264165226080"
+    }
+};
