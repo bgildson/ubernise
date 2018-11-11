5fe7df6dbbc891e3740a705e704dba28b16aebd7
bgildson <gildson13@hotmail.com>
2018-11-11 09:39:24 -0300

    undo last commit created to intercall calls

--- functions/src/functions/carteiras.ts
+++ functions/src/functions/carteiras.ts
@@ -8,59 +8,53 @@ import { authenticatedAndUsuarioWithPerfilIn } from './shared';
 
 export const carteirasAdicionarCredito = functions.https.onCall(
   async (data: { usuario_uid; valor }, context) => {
-    try {
-      await authenticatedAndUsuarioWithPerfilIn(context, [
-        'administrador',
-        'motorista'
-      ]);
+    await authenticatedAndUsuarioWithPerfilIn(context, [
+      'administrador',
+      'motorista'
+    ]);
 
-      const _data = new Date();
-      const valor = parseStringToNumber(data.valor);
+    const _data = new Date();
+    const valor = parseStringToNumber(data.valor);
 
-      if (valor <= 0)
-        throw new functions.https.HttpsError(
-          'failed-precondition',
-          'O valor deve ser maior que zero!'
-        );
+    if (valor <= 0)
+      throw new functions.https.HttpsError(
+        'failed-precondition',
+        'O valor deve ser maior que zero!'
+      );
 
-      const carteiraSnap = await CarteirasService.getById(
-        data.usuario_uid
-      ).get();
-      if (!carteiraSnap.exists)
-        throw new functions.https.HttpsError(
-          'not-found',
-          `Carteira "${data.usuario_uid}" não encontrada!`
-        );
-      const carteira = <CarteiraModel>carteiraSnap.data();
+    const carteiraSnap = await CarteirasService.getById(data.usuario_uid).get();
+    if (!carteiraSnap.exists)
+      throw new functions.https.HttpsError(
+        'not-found',
+        `Carteira "${data.usuario_uid}" não encontrada!`
+      );
+    const carteira = <CarteiraModel>carteiraSnap.data();
 
-      const batch = firebase.firestore().batch();
+    const batch = firebase.firestore().batch();
 
-      const _carteira = <CarteiraModel>{
-        ...carteira,
-        id: carteiraSnap.id,
-        saldo: carteira.saldo + valor
-      };
-      batch.update(carteiraSnap.ref, _carteira);
+    const _carteira = <CarteiraModel>{
+      ...carteira,
+      id: carteiraSnap.id,
+      saldo: carteira.saldo + valor
+    };
+    batch.update(carteiraSnap.ref, _carteira);
 
-      const movimentacaoRef = MovimentacoesService.createRef();
-      const movimentacao: MovimentacaoModel = {
-        id: movimentacaoRef.id,
-        usuario_uid: carteiraSnap.id,
-        data: _data,
-        tipo: 'entrada',
-        operacao: 'credito',
-        valor
-      };
+    const movimentacaoRef = MovimentacoesService.createRef();
+    const movimentacao: MovimentacaoModel = {
+      id: movimentacaoRef.id,
+      usuario_uid: carteiraSnap.id,
+      data: _data,
+      tipo: 'entrada',
+      operacao: 'credito',
+      valor
+    };
 
-      batch.create(movimentacaoRef, movimentacao);
+    batch.create(movimentacaoRef, movimentacao);
 
-      await batch.commit();
+    await batch.commit();
 
-      // send notification
+    // send notification
 
-      return _carteira;
-    } catch (err) {
-      throw new functions.https.HttpsError(err.code || 'internal', err.message);
-    }
+    return _carteira;
   }
 );
--- functions/src/functions/taxas.ts
+++ functions/src/functions/taxas.ts
@@ -9,107 +9,99 @@ import { authenticatedAndUsuarioWithPerfilIn } from './shared';
 
 export const taxasCreate = functions.https.onCall(
   async (taxa: { valor; data_inicial }, context) => {
-    try {
-      await authenticatedAndUsuarioWithPerfilIn(context, [
-        'administrador',
-        'motorista'
-      ]);
-
-      let _taxa = <Partial<TaxaModel>>{
-        valor: parseStringToNumber(taxa.valor),
-        data_inicial: moment(taxa.data_inicial).toDate()
-      };
-
-      const batch = firebase.firestore().batch();
-
-      const taxaAnteriorSnap = await TaxasService.getByDataFinal(null).get();
-      if (!taxaAnteriorSnap.empty) {
-        const taxaAnterior = <TaxaModel>taxaAnteriorSnap.docs[0].data();
-
-        if (taxaAnterior.data_inicial >= _taxa.data_inicial)
-          throw new functions.https.HttpsError(
-            'failed-precondition',
-            'Não foi possível criar a taxa, pois existe uma outra que inicia após esta!'
-          );
-
-        const dataFinalAnterior = moment(taxa.data_inicial)
-          .subtract(1, 'days')
-          .toDate();
-
-        batch.update(taxaAnteriorSnap.docs[0].ref, <TaxaModel>{
-          ...taxaAnterior,
-          data_final: dataFinalAnterior
-        });
-      }
-
-      _taxa = <TaxaModel>{
-        ..._taxa,
-        data_final: null
-      };
-      const taxaRef = TaxasService.createRef();
-      batch.create(taxaRef, _taxa);
-
-      await batch.commit();
-
-      return {
-        ..._taxa,
-        id: taxaRef.id,
-        data_inicial: _taxa.data_inicial.toISOString()
-      };
-    } catch (err) {
-      throw new functions.https.HttpsError(err.code || 'internal', err.message);
-    }
-  }
-);
-
-export const taxasDelete = functions.https.onCall(async (taxaId, context) => {
-  try {
     await authenticatedAndUsuarioWithPerfilIn(context, [
       'administrador',
       'motorista'
     ]);
 
-    const viagensSnap = await ViagensService.getByTaxaId(taxaId).get();
-    if (!viagensSnap.empty)
-      throw new functions.https.HttpsError(
-        'failed-precondition',
-        'Não foi possível deletar a taxa, já existem viagens utilizando esta taxa!'
-      );
-
-    const taxaSnap = await TaxasService.getById(taxaId).get();
-    if (!taxaSnap.exists)
-      throw new functions.https.HttpsError(
-        'not-found',
-        `Taxa "${taxaId}" não foi encontrada!`
-      );
-    const taxa = <TaxaModel>taxaSnap.data();
-
-    if (taxa.data_final !== null)
-      throw new functions.https.HttpsError(
-        'failed-precondition',
-        'Você só pode excluir a taxa mais recente!'
-      );
+    let _taxa = <Partial<TaxaModel>>{
+      valor: parseStringToNumber(taxa.valor),
+      data_inicial: moment(taxa.data_inicial).toDate()
+    };
 
     const batch = firebase.firestore().batch();
 
-    const taxaAnteriorDataFinal = moment(taxa.data_inicial)
-      .subtract(1, 'days')
-      .toDate();
-    const taxaAnteriorSnap = await TaxasService.getByDataFinal(
-      taxaAnteriorDataFinal
-    ).get();
-    if (!taxaAnteriorSnap.empty)
-      batch.update(taxaAnteriorSnap.docs[0].ref, <TaxaModel>{
-        ...taxaAnteriorSnap.docs[0].data(),
-        data_final: null
-      });
+    const taxaAnteriorSnap = await TaxasService.getByDataFinal(null).get();
+    if (!taxaAnteriorSnap.empty) {
+      const taxaAnterior = <TaxaModel>taxaAnteriorSnap.docs[0].data();
 
-    batch.delete(taxaSnap.ref);
+      if (taxaAnterior.data_inicial >= _taxa.data_inicial)
+        throw new functions.https.HttpsError(
+          'failed-precondition',
+          'Não foi possível criar a taxa, pois existe uma outra que inicia após esta!'
+        );
+
+      const dataFinalAnterior = moment(taxa.data_inicial)
+        .subtract(1, 'days')
+        .toDate();
+
+      batch.update(taxaAnteriorSnap.docs[0].ref, <TaxaModel>{
+        ...taxaAnterior,
+        data_final: dataFinalAnterior
+      });
+    }
+
+    _taxa = <TaxaModel>{
+      ..._taxa,
+      data_final: null
+    };
+    const taxaRef = TaxasService.createRef();
+    batch.create(taxaRef, _taxa);
 
     await batch.commit();
 
-    return true;
-  } catch (err) {
-    throw new functions.https.HttpsError(err.code || 'internal', err.message);
+    return {
+      ..._taxa,
+      id: taxaRef.id,
+      data_inicial: _taxa.data_inicial.toISOString()
+    };
   }
+);
+
+export const taxasDelete = functions.https.onCall(async (taxaId, context) => {
+  await authenticatedAndUsuarioWithPerfilIn(context, [
+    'administrador',
+    'motorista'
+  ]);
+
+  const viagensSnap = await ViagensService.getByTaxaId(taxaId).get();
+  if (!viagensSnap.empty)
+    throw new functions.https.HttpsError(
+      'failed-precondition',
+      'Não foi possível deletar a taxa, já existem viagens utilizando esta taxa!'
+    );
+
+  const taxaSnap = await TaxasService.getById(taxaId).get();
+  if (!taxaSnap.exists)
+    throw new functions.https.HttpsError(
+      'not-found',
+      `Taxa "${taxaId}" não foi encontrada!`
+    );
+  const taxa = <TaxaModel>taxaSnap.data();
+
+  if (taxa.data_final !== null)
+    throw new functions.https.HttpsError(
+      'failed-precondition',
+      'Você só pode excluir a taxa mais recente!'
+    );
+
+  const batch = firebase.firestore().batch();
+
+  const taxaAnteriorDataFinal = moment(taxa.data_inicial)
+    .subtract(1, 'days')
+    .toDate();
+  const taxaAnteriorSnap = await TaxasService.getByDataFinal(
+    taxaAnteriorDataFinal
+  ).get();
+  if (!taxaAnteriorSnap.empty)
+    batch.update(taxaAnteriorSnap.docs[0].ref, <TaxaModel>{
+      ...taxaAnteriorSnap.docs[0].data(),
+      data_final: null
+    });
+
+  batch.delete(taxaSnap.ref);
+
+  await batch.commit();
+
+  return true;
 });
